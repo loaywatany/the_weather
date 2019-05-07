28b619f8930ec0f36799b106c42c5591c3487bc6
Hesham Watany <hesham.watany@gmail.com>
2019-05-07 21:22:45 +0200

    Added the models to the function & the template

--- weather/views.py
+++ weather/views.py
@@ -1,17 +1,21 @@
 import requests
 from django.shortcuts import render
-
+from .models import City
 
 def index(request):
     url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=2b7e7ab4395541013bb0df19fba94fac'
     city = 'cairo'
-    r = requests.get(url.format(city)).json()
-    city_weather = {
-        'city': city,
-        'temperature': r['main']['temp'],
-        'icon': r['weather'][0]['icon'],
-    }
-    context = {
-        'city_weather': city_weather
-    }
+    cities = City.objects.all()
+    weather_data = []
+    for city in cities:
+        r = requests.get(url.format(city)).json()
+        city_weather = {
+            'city': city,
+            'temperature': r['main']['temp'],
+            'icon': r['weather'][0]['icon'],
+        }
+
+        weather_data.append(city_weather)
+
+    context = {'weather_data': weather_data}
     return render(request, 'weather/index.html', context)
