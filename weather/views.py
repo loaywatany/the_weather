f0f3fbbf6911c2e882bb2823bda93ea0689dd652
Hesham Watany <hesham.watany@gmail.com>
2019-05-07 20:56:19 +0200

    Added new index function

--- weather/views.py
+++ weather/views.py
@@ -1,3 +1,17 @@
+import requests
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=2b7e7ab4395541013bb0df19fba94fac'
+    city = 'cairo'
+    r = requests.get(url.format(city)).json()
+    city_weather = {
+        'city': city,
+        'temperature': r['main']['temp'],
+        'icon': r['weather'][0]['icon'],
+    }
+    context = {
+        'city_weather': city_weather
+    }
+    return render(request, 'weather/index.html', context)
